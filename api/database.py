29dab58b6f8ba03695442f3953b6db49e1c90ae3
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-05 13:41:19 -0600

    Update database.py

--- api/database.py
+++ api/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 _user = "root"
-_password = "a1b2c3d4"
+_password = "Test@12345"
 _host = "127.0.0.1"
 _port = 3306
 _database = "auditly"
