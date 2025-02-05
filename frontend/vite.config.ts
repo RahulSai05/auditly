bc9b3a371e27b7d9c9f5811763adef41d51c2adc
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-05 15:24:58 -0600

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-server: {
-    open: false
-  },
-  plugins: [react()]
-});
+export default defineConfig({
+  plugins: [react()],
+})
