f4be0b36d4c36e301aa271a097fdbfa34b7a517a
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-05 14:10:39 -0600

    Update app.py

--- api/app.py
+++ api/app.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React frontend URL
+    allow_origins=["*"],  # React frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
