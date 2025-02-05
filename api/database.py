4579a4af4c63ef89c13238218dabe6d3d827c12f
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-05 14:04:45 -0600

    Update database.py

--- api/database.py
+++ api/database.py
@@ -1,9 +1,10 @@
+import urllib.parse
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 _user = "root"
-_password = "Test@12345"
+_password = urllib.parse.quote_plus("Test@12345")  # URL encode the password
 _host = "127.0.0.1"
 _port = 3306
 _database = "auditly"
@@ -14,4 +15,3 @@ DATABASE_URL = f"mysql+pymysql://{_user}:{_password}@{_host}:{_port}/{_database}
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
