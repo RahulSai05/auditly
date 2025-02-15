55933ad0384dade5f8c84b964dd45d3bd16d2cc7
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-15 17:38:18 -0600

    Update send_email.py

--- api/send_email.py
+++ api/send_email.py
@@ -1,8 +1,11 @@
 import smtplib
+import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
-def send_email(sender_email, sender_password, recipient_email, subject, message):
+def send_email(sender_email, sender_password, recipient_email, subject, message, image_list):
     """
     Sends an email using an SMTP server.
     
@@ -26,6 +29,14 @@ def send_email(sender_email, sender_password, recipient_email, subject, message)
         # Attach the email body
         msg.attach(MIMEText(message, 'plain'))
 
+        for image_path in image_list:
+            with open(image_path, "rb") as img_file:
+                    mime_base = MIMEBase("application", "octet-stream")
+                    mime_base.set_payload(img_file.read())
+                    encoders.encode_base64(mime_base)
+                    mime_base.add_header("Content-Disposition", f"attachment; filename={image_path[-5:]}")
+                    msg.attach(mime_base)
+                    
         # Connect to the SMTP server and send the email
         with smtplib.SMTP('smtp.gmail.com', 587) as server:
             server.starttls()  # Secure the connection
