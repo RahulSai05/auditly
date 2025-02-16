cb257f64d69e4def049313f7249b32d3b9561c8e
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-16 10:13:42 -0600

    Update Options.tsx

--- frontend/src/pages/Options.tsx
+++ frontend/src/pages/Options.tsx
@@ -91,7 +91,6 @@
 // };
 
 // export default Options;
-
 import { useNavigate } from "react-router-dom";
 import { ScanQrCode, NotebookPen } from "lucide-react";
 
@@ -103,7 +102,7 @@ const Options = () => {
       <h2 className="text-3xl mb-6">Choose the best Option for your needs</h2>
       <div className="grid md:flex gap-6">
         {/* Quick Scan Option */}
-        <div className="w-full md:w-[390px] border p-6 hover:text-blue-600 bg-[#F9FAFB] shadow-2xl rounded-lg flex flex-col items-center text-center">
+        <div className="w-full md:w-[390px] border border-gray-300 p-6 hover:border-blue-600 bg-[#F9FAFB] shadow-2xl rounded-lg flex flex-col items-center text-center transition-colors">
           <ScanQrCode size={90} className="text-current" />
           <h3 className="text-gray-800 text-xl my-5">Quick Scan</h3>
           <p className="text-gray-800 mb-4">
@@ -111,14 +110,14 @@ const Options = () => {
           </p>
           <button
             onClick={() => router("/auto/scan")}
-            className="mt-auto bg-transparent hover:bg-[#5986E7] border border-transparent hover:text-white px-4 py-2 rounded-lg transition-colors"
+            className="mt-auto bg-transparent border border-blue-600 hover:bg-blue-600 hover:text-white px-4 py-2 rounded-lg transition-colors"
           >
             Proceed with Quick Scan
           </button>
         </div>
 
         {/* Manual Entry Option */}
-        <div className="w-full md:w-[390px] hover:text-blue-600 border p-6 bg-[#F9FAFB] shadow-2xl rounded-lg flex flex-col items-center text-center">
+        <div className="w-full md:w-[390px] border border-gray-300 p-6 hover:border-blue-600 bg-[#F9FAFB] shadow-2xl rounded-lg flex flex-col items-center text-center transition-colors">
           <NotebookPen size={90} className="text-current" />
           <h3 className="text-gray-800 text-xl my-5">Enter Manually</h3>
           <p className="text-gray-800 mb-4">
@@ -126,7 +125,7 @@ const Options = () => {
           </p>
           <button
             onClick={() => router("/option/manual")}
-            className="mt-auto bg-transparent hover:bg-[#5986E7] border border-transparent hover:text-white px-4 py-2 rounded-lg transition-colors"
+            className="mt-auto bg-transparent border border-blue-600 hover:bg-blue-600 hover:text-white px-4 py-2 rounded-lg transition-colors"
           >
             Proceed with Manual Entry
           </button>
