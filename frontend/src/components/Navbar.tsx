cb2b308d0e8a3f817016c54f9af55281afa157c1
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-17 16:11:36 -0600

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -196,8 +196,9 @@ export function Navbar() {
                 </motion.li>
                 <motion.li whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
                   <Link
-                    to="/admin/reports/items"
-                    className="hover:text-blue-600 flex items-center gap-x-3 text-sm md:text-base transition"
+{/*                     to="/admin/reports/items"
+                    to="/admin"
+                    className="hover:text-blue-600 flex items-center gap-x-3 text-sm md:text-base transition" */}
                     onClick={() => setIsOpen(false)}
                   >
                     <Lock className="w-5 h-5" />
