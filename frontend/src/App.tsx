efe11167e6546ccc47d0e1c40c7dd5fee4bed02b
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-18 13:59:57 -0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -69,14 +69,14 @@ export default function App() {
         {/* Admin Routes with AdminLayout */}
         <Route element={<AdminLayout />}>
           <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/settings/Item master upload" element={<ItemUpload />} />
-          <Route path="/admin/settings/Item Image Upload" element={<ItemImageUpload />} />
+          <Route path="/admin/settings/Item-master-upload" element={<ItemUpload />} />
+          <Route path="/admin/settings/Item-Image-Upload" element={<ItemImageUpload />} />
           <Route path="/admin/reports/items" element={<DashboardTables />} />
           <Route path="/admin/reports/customer-serials" element={<CustomerSerials />} />
           <Route path="/admin/reports/returns" element={<ReturnDetails />} />
-          <Route path="/admin/settings/Return Upload" element={<ItemReturn />} />
+          <Route path="/admin/settings/Return-Upload" element={<ItemReturn />} />
           <Route path="/admin/reports/audity-inspections" element={<AuditlyInspection />} />
-          <Route path="/admin/settings/customer serial Upload" element={<CustomerSerialUpload />} />
+          <Route path="/admin/settings/customer-serial-Upload" element={<CustomerSerialUpload />} />
           <Route path="/admin/settings/users-maintenance" element={<UserMaintenance />} />   
           <Route path="/admin/settings/connectors/inbound" element={<Inbound />} />   
           <Route path="/admin/settings/connectors/outbound" element={<Outbound />} />   
