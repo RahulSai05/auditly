2fd05c1a424991ae7e3486f026232a86012141e6
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-18 10:48:24 -0600

    Create CustomerSerialUpload.jsx

---
+++ frontend/src/pages/admin/CustomerSerialUpload.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CustomerSerialUpload = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a JSX file.</p>
+    </div>
+  );
+};
+
+export default CustomerSerialUpload;
