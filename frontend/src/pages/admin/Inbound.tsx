318aff8dd1c7f1f92162c3cc92ecf432b049bc3f
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-18 12:16:18 -0600

    Update Inbound.tsx

--- frontend/src/pages/admin/Inbound.tsx
+++ frontend/src/pages/admin/Inbound.tsx
@@ -1,11 +1,13 @@
-
 import React from 'react';
 
 function Inbound() {
   return (
     <div className="App">
       <header className="App-header">
-        <Greeting />
+        {/* Display "Hello, World!" directly */}
+        <p>Hello, World!</p>
+        {/* If you still need the Greeting component, uncomment the next line */}
+        {/* <Greeting /> */}
       </header>
     </div>
   );
