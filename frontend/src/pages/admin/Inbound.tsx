56ea7a7c0417f78799d4ade48c599561a852fa51
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-18 12:12:26 -0600

    Update Inbound.tsx

--- frontend/src/pages/admin/Inbound.tsx
+++ frontend/src/pages/admin/Inbound.tsx
@@ -1,1 +1,16 @@
 
+import React from 'react';
+import './App.css';
+import Greeting from './Greeting'; // Ensure the path matches where you saved Greeting.jsx
+
+function Inbound() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Greeting />
+      </header>
+    </div>
+  );
+}
+
+export default Inbound;
