2e869251b44dbfe5dfa36ffd3f16f87b48ad43d9
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-02-19 15:28:46 -0600

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -37,7 +37,7 @@ export function Navbar() {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         >
-          <div className="bg-gradient-to-r from-blue-600 to-blue-700 bg-clip-text text-transparent" style={{ fontFamily: 'Arial, sans-serif' }}>
+          <div className="bg-gradient-to-r from-blue-600 to-blue-700 bg-clip-text text-transparent" style={{ fontFamily: 'Satisfy, cursive' }}>
             auditly
           </div>
           <span className="text-black" style={{ fontFamily: 'Dancing Script, cursive', fontSize: '1.3em' }}>.</span>
