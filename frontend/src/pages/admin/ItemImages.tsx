8568868c1289ef1271e0b88fe8c7a31790075afd
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-03-13 19:37:06 -0500

    Update ItemImages.tsx

--- frontend/src/pages/admin/ItemImages.tsx
+++ frontend/src/pages/admin/ItemImages.tsx
@@ -1,1 +1,12 @@
 
+import React from "react";
+
+const ItemImages: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen text-2xl font-bold">
+      Hello, World!
+    </div>
+  );
+};
+
+export default ItemImages;
