c8c70743e1f9fb5e7d32348a51bb70ca62329410
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-03-27 10:54:50 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2089,7 +2089,7 @@ async def powerbi_callback(request: Request, db: Session = Depends(get_db)):
             "powerbi_user_email": user_data['email']
         })
 
-        return RedirectResponse(url="https://auditlyai.com/dashboard"")
+        return RedirectResponse(url="https://auditlyai.com/dashboard")
         
     except OAuthError as e:
         db.rollback()
