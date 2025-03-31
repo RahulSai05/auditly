f0e577f9b6362dedbf2a4e5af227d8f143c9f02e
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-03-31 16:00:17 -0500

    Update LoadingSpinner.tsx

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CircularProgress } from "@mui/material"; // If using Material-UI
 
 const LoadingSpinner: React.FC = () => {
   return (
@@ -8,9 +9,17 @@ const LoadingSpinner: React.FC = () => {
       alignItems: "center",
       height: "100vh",
       width: "100vw",
-      backgroundColor: "#f8f9fa"
+      backgroundColor: "#f8f9fa",
+      position: "fixed",
+      top: 0,
+      left: 0,
+      zIndex: 9999
     }}>
-      <div>Loading...</div>
+      <div style={{ textAlign: "center" }}>
+        {/* You can replace this with any spinner component */}
+        <CircularProgress />
+        <div style={{ marginTop: "16px" }}>Loading...</div>
+      </div>
     </div>
   );
 };
