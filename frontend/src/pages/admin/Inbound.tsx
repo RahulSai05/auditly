8bd8be36251c99689f396c9f840570b8189d5408
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-04-23 12:23:14 -0500

    Update Inbound.tsx

--- frontend/src/pages/admin/Inbound.tsx
+++ frontend/src/pages/admin/Inbound.tsx
@@ -1546,9 +1546,9 @@ const Inbound: React.FC = () => {
                           onClick={() => toggleCardExpand(source.id)}
                           className="text-gray-500 hover:text-gray-700 transition-colors"
                         >
-{/*                           {expandedCards[source.id] ? <ChevronUp className="w-5 h-5" /> : <ChevronDown className="w-5 h-5" />} */}
-                        {expandedCardId === source.id ? <ChevronUp className="w-5 h-5" /> : <ChevronDown className="w-5 h-5" />}
-
+                        {expandedCards[source.id] ? <ChevronUp className="w-5 h-5" /> : <ChevronDown className="w-5 h-5" />}
+{/*                         {expandedCardId === source.id ? <ChevronUp className="w-5 h-5" /> : <ChevronDown className="w-5 h-5" />}
+ */}
                         </button>
                       </div>
 
