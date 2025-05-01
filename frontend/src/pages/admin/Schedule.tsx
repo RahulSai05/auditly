16aa1b4cf105b72d8c33301016597a8ff53c93b7
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-05-01 10:11:08 -0500

    Create Schedule.tsx

---
+++ frontend/src/pages/admin/Schedule.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import React from "react";
+import HelloWorld from "./HelloWorld";
+
+const Schedule: React.FC = () => {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default Schedule;
