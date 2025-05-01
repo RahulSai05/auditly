2291b31a850546338ffb83cbad2999de3bf29832
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-05-01 10:58:55 -0500

    Create ScheduledDeliveries.tsx

---
+++ frontend/src/pages/agent/ScheduledDeliveries.tsx
@@ -0,0 +1,12 @@
+// HelloWorld.tsx
+import React from "react";
+
+const ScheduledDeliveries: React.FC = () => {
+  return (
+    <div style={{ padding: "20px", fontSize: "24px", fontWeight: "bold" }}>
+      Hello World
+    </div>
+  );
+};
+
+export default ScheduledDeliveries;
