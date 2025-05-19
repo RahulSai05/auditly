849dac8352af41deed9f77eb9327103f5729d783
RahulSai05 <100005652+RahulSai05@users.noreply.github.com>
2025-05-19 11:11:07 -0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allow external access
-    port: 5173, // Ensure the port is the same as the one you are using
+    host: '0.0.0.0',
+    port: 5173,
     strictPort: true,
     watch: {
       usePolling: true,
-    }
-  }
-})
+    },
+    proxy: {
+      '/api': {
+        target: 'http://auditlyai.com:8000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '/api'),
+      },
+    },
+  },
+});
